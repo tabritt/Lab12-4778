df9b45c662da418e44588f6328e9c11f5ef6465e
tkpleo <tkristofferpurdy@gmail.com>
2025-11-09 22:35:31 -0500

    changed access type for sortedlist

--- Lab12-Dig4778/Assets/Scripts/InventoryManager.cs
+++ Lab12-Dig4778/Assets/Scripts/InventoryManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 { 
     public List<InventoryItem> items = new List<InventoryItem>();
-    List<InventoryItem> sortedList = new List<InventoryItem>();
+    public List<InventoryItem> sortedList = new List<InventoryItem>();
     public int itemIdQuery = 0;
 
     void Start()
